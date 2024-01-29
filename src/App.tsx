71f3f12e7993723858a6666bbc8c7921bdf50fa6
Priyanshi17jan <priyanshishalya@gmail.com>
2024-01-29 15:07:33 +0530

    chnages

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import DoctorForm from './Components/DoctorForm';
+// import AppointmentForm from './Components/Appointment';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    
+      <DoctorForm />
+      {/* <AppointmentForm/> */}
+    </div>
+  );
+}
+
+export default App;
